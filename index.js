1688fd1b13958564ddf0ecca7fe3d99c12bd766a
Guillaume Banyuls <guillau20@gmail.com>
2024-05-03 17:21:13 +0200

    Terminado el fondo del header y el titulo + animacion

---
+++ index.js
@@ -0,0 +1,41 @@
+function randomizeIcons() {
+  const container = document.querySelector('.icon-container');
+  const originalIcons = document.querySelectorAll('.icon');
+  
+
+  originalIcons.forEach(icon => {
+    // Duplicate each icon twice
+    for (let i = 0; i < 15; i++) {
+      const duplicateIcon = icon.cloneNode(true);
+      container.appendChild(duplicateIcon);
+    }
+  });
+
+
+
+    const icons = document.querySelectorAll('.icon');
+
+    icons.forEach(icon => {
+
+      const x = Math.random() * 100 - 50 + 'vw';
+      const y = Math.random() * 17 + 15 + 'vh';
+      icon.style.left = x;
+      icon.style.top = y;
+
+     
+   
+
+
+      
+      const floatDuration = Math.random() * 30 + 80;
+
+      setTimeout(() => {
+       
+        icon.style.animation = `floatAway ${floatDuration}s linear infinite`; 
+        container.style.opacity = "1";
+      }, 2000); 
+    });
+    
+  }
+
+  window.onload = randomizeIcons; // Call function on page load
